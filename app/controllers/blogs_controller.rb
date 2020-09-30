ea731da515d0e23d7d7ff9fd4f8d11cd87897095
Leo Carey-Williams <leocareywilliams@gmail.com>
2020-09-30 18:13:49 +0100

    blog specific content added to show page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -55,6 +55,6 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :date, :content, :mp3, photos: [])
+    params.require(:blog).permit(:title, :date, :content, :contentMain, :mp3, photos: [])
   end
 end
